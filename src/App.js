bdf7d5d2c4352101c4422397ec2ec413b16cb735
GiulianiTobon <giuliani.1905@hotmail.com>
2023-05-26 23:13:11 -0500

    Actualización

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from '../components/Navbar';
 import HomePage from '../components/HomePage';
 import RestaurantSearch from '../components/RestaurantSearch';
 import NewRestaurantForm from '../components/NewRestaurantForm';
@@ -7,6 +8,7 @@ import NewRestaurantForm from '../components/NewRestaurantForm';
 function App() {
   return (
     <Router>
+      <Navbar />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/search" component={RestaurantSearch} />
